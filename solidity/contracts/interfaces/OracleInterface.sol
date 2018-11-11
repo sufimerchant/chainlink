3224b571a21dea523cf0a89cf9ea55bd4bab7273
Alex Coventry <coventry@gmail.com>
2018-11-11 15:56:41 -0500

    Remove cancel affordance

--- solidity/contracts/interfaces/OracleInterface.sol
+++ solidity/contracts/interfaces/OracleInterface.sol
@@ -4,5 +4,4 @@ interface OracleInterface {
   function fulfillData(uint256 _internalId, bytes32 _data) external returns (bool);
   function fulfillServiceAgreement(uint256 _internalId, bytes32 _data) external returns (bool);
   function cancel(bytes32 _externalId) external;
-  function cancelServiceAgreement(bytes32 _externalId) external;
 }
